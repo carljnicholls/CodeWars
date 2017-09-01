64bee8fc6cf553848cf9b1984e2cac1cc0d4ab7d
Carl Nicholls <carljnicholls@hotmail.com>
2017-09-01 21:15:31 +0100

    ListFilterer project added

---
+++ ListFilterer.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ListFilterer.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ListFilterer.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49f75dbb-cd17-47f6-9d93-4a9c9b9b7bb7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ListFilterer/ListFilterer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListFilterer
+{
+    public class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<int> listOfInt = new List<int>();
+
+            for (int i = 0; i < listOfItems.Count; i++)
+            {
+                Type type = listOfItems[i].GetType();
+                if (type == typeof(int))
+                    listOfInt.Add((int)listOfItems[i]);
+            }
+
+            return listOfInt;
+        }
+    }
+}
