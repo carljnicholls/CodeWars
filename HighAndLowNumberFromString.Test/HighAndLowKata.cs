f616be9cb52915e394c16617f08f4e13a1eeff9f
Carl Nicholls <carljnicholls@hotmail.com>
2017-09-01 20:51:27 +0100

    High And Low Kata Complete - this kata passed a string para and needed to return the highest and lowest number. ListFilterer Kata Complete - this kata takes a list of object and only returns int primitives

--- HighAndLowNumberFromString.Test/HighAndLowKata.cs
+++ HighAndLowNumberFromString.Test/HighAndLowKata.cs
@@ -9,75 +9,75 @@ namespace HighAndLowNumberFromString.Test
     public class HighAndLowKata
     {
         [Test]
-        public void Test1()
+        public void HighAndLow_Test1()
         {
             Assert.AreEqual("42 -9", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
         }
         [Test]
-        public void Test2()
+        public void HighAndLow_Test2()
         {
             Assert.AreEqual("5 1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 2 3 4 5"));
         }
         [Test]
-        public void Test3()
+        public void HighAndLow_Test3()
         {
             Assert.AreEqual("5 -3", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 2 -3 4 5"));
         }
         [Test]
-        public void Test4()
+        public void HighAndLow_Test4()
         {
             Assert.AreEqual("9 -5", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 9 3 4 -5"));
         }
         [Test]
-        public void SomeTest()
+        public void HighAndLow_SomeTest()
         {
             Assert.AreEqual("542 -214", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
         }
 
         [Test]
-        public void PlusMinusTest()
+        public void HighAndLow_PlusMinusTest()
         {
             Assert.AreEqual("1 -1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 -1"));
         }
 
         [Test]
-        public void PlusPlusTest()
+        public void HighAndLow_PlusPlusTest()
         {
             Assert.AreEqual("1 1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 1"));
         }
 
         [Test]
-        public void MinusMinusTest()
+        public void HighAndLow_MinusMinusTest()
         {
             Assert.AreEqual("-1 -1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("-1 -1"));
         }
 
         [Test]
-        public void PlusMinusZeroTest()
+        public void HighAndLow_PlusMinusZeroTest()
         {
             Assert.AreEqual("1 -1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 -1 0"));
         }
 
         [Test]
-        public void PlusPlusZeroTest()
+        public void HighAndLow_PlusPlusZeroTest()
         {
             Assert.AreEqual("1 0", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("1 1 0"));
         }
 
         [Test]
-        public void MinusMinusZeroTest()
+        public void HighAndLow_MinusMinusZeroTest()
         {
             Assert.AreEqual("0 -1", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("-1 -1 0"));
         }
 
         [Test]
-        public void SingleTest()
+        public void HighAndLow_SingleTest()
         {
             Assert.AreEqual("42 42", HighAndLowNumberFromString.HighAndLowKata.HighAndLow("42"));
         }
 
         [Test]
-        public void RandomTest()
+        public void HighAndLow_RandomTest()
         {
             Random r = new Random();
             List<int> numbers = new List<int>();
